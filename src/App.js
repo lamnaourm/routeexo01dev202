4f2a1b85c9c2ee9d1f68c5b1117667cbb49e79d9
lamnaourm <mohammed.lamnaour@gmail.com>
2023-11-07 10:27:32 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route index element={<Home /> } />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </div>
   );
 }
